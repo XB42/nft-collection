40ecc0a064e91385f8c710a04cf96a9464fc6494
Bux Khurana <53265818+XB42@users.noreply.github.com>
2022-10-25 15:40:43 +0400

    file upload

---
+++ hardhat-tutorial/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x44F06ED59d1e2C4f36F7327eB872CEc9723ACD86";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
