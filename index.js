6a9b75d60dd0e9b215e92b25e514d4098de6f7cf
Bux Khurana <53265818+XB42@users.noreply.github.com>
2022-10-25 19:00:56 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x44F06ED59d1e2C4f36F7327eB872CEc9723ACD86";
+const WHITELIST_CONTRACT_ADDRESS = "0xA9e70b80652d0A5dFDc0cce1E60AaB1ABd9b715e";
 // URL to extract Metadata for a Crypto Dev NFT
 const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
 
